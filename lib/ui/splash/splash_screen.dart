954e5fc50681ac9952c94bf533546d451fc3436e
gha211th <agh.arrafif@gmail.com>
2026-01-22 08:55:24 +0700

    build a splash screen

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,10 +1,76 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'dart:async';
+import '../dashboard/dashboard_screen.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const DashboardScreen()),
+      );
+    });
+  }
+
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xFF0077B6),
+      body: Center(
+        child: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Sregep",
+                  style: GoogleFonts.outfit(
+                    color: Color(0xFFFFFFFF),
+                    fontSize: 48,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  "[ Focus - Study - Achieve ]",
+                  style: GoogleFonts.outfit(
+                    color: Color(0xFFFFFFFF),
+                    fontSize: 11,
+                    fontWeight: FontWeight.w300,
+                    letterSpacing: 1,
+                  ),
+                ),
+              ],
+            ),
+            Positioned(
+              bottom: 70,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Text(
+                  "Loading....",
+                  style: GoogleFonts.outfit(
+                    color: Color(0xFFFFFFFF),
+                    fontSize: 11,
+                    fontWeight: FontWeight.w300,
+                    letterSpacing: sqrt1_2,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
